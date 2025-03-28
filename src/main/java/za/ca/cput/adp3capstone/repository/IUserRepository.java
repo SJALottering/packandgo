4adfabff00e2e4accb5ebc73fe0091f319a80725
EthanLeRoux <127761539+EthanLeRoux@users.noreply.github.com>
2025-03-28 20:10:19 +0200

    Edited IUserRepository

--- src/main/java/za/ca/cput/adp3capstone/repository/IUserRepository.java
+++ src/main/java/za/ca/cput/adp3capstone/repository/IUserRepository.java
@@ -2,14 +2,11 @@ package za.ca.cput.adp3capstone.repository;
 
 import za.ca.cput.adp3capstone.domain.User;
 
+import java.util.HashMap;
 import java.util.Optional;
 
-    // Repository Interface
-    public interface IUserRepository {
-        void save(User user);
-        Optional<User> findById(String id);
-        void deleteById(String id);
-
+    public interface IUserRepository extends IRepository<User, String>{
+        HashMap<String, User> getAll();
     }
 
 
